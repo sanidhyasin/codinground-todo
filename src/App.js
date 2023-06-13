bfd793a1d05abaa795a192b46ffae12ac6cd39dc
sanidhyasin <singhsanidhya741@gmail.com>
2023-06-13 19:10:12 +0530

    first code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDo from "./Todo";
+
+const App = () => {
+  return (
+    <div>
+      <ToDo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,60 @@
+import React, { useReducer, useState } from "react";
+import TodoCard from "./TodoCard";
+export const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo",
+};
+
+function reducer(todos, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos;
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
+const Todo = () => {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault(); //prevents page from refreshing
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    //payload generally contains the variables or the values that we need to perform that action
+    setName("");
+  }
+
+  console.log(todos);
+  return (
+    <>
+    <h1>todo app</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => {
+        return <TodoCard key={todo.id} todo={todo} dispatch={dispatch} />;
+      })}
+    </>
+  );
+};
+
+export default Todo;
